5c7eb98f978a486e28553c26f130b20ff757f449
Pedro <pbentoit@gmail.com>
2023-03-29 21:10:49 +0100

    implemented opening prompt with exeption handling started value gather function

--- FizzBang/FizzBang.py
+++ FizzBang/FizzBang.py
@@ -1,15 +1,28 @@
 # coding: utf-8
 
 import os
+from distutils.util import strtobool
 
 def FizzBang():
     while True:
-        option = input("Hello! Would you like to play FizzBang? (y/n)")
-        if option == "y":
-            print("")
-        elif option == "n":
-            print("")
-        else:
-            print("Goodbye")
+        try:
+            option = strtobool(input("Hello! Would you like to play FizzBang? (y/n)"))
+        except ValueError:
+            print("\nSorry, I didn't understand!\nYou should reply with yes or no!\n")
             os.system("pause")
-            quit()
+            os.system('cls')
+        else:
+            if option == 1:
+                print("\nGreat! I love this game!\n")
+                get_range()
+            elif option == 0:
+                print("\nThat's too bad, let's play another time then!\n Goodbye!")
+                os.system("pause")
+                quit()          
+
+def get_range():
+    while True:
+        print("range")
+        break
+
+FizzBang()
