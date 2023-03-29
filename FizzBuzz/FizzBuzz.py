622f1011ff8043fa281ad8a18a0152f945bfce35
Pedro <pbentoit@gmail.com>
2023-03-29 22:07:27 +0100

    updated range functions

--- FizzBuzz/FizzBuzz.py
+++ FizzBuzz/FizzBuzz.py
@@ -24,16 +24,18 @@ def FizzBang():
 
 def get_values():
     while True:
-        print("\nTo start, we will need to set the range!\n\nRemember both the begining and the end should be Integers,")
-        print("and the end should be higher than the begining!\n")
+        print("\nTo start, we will need to set the range!\n\nRemember both the begining and the end should be Integers,\nand the end should be higher than the start!\n")
         os.system("pause")
         os.system('cls')
-        print("What is the begining?")
+        print("What is the start number?")
         min = get_int()
-        print("Great! We will start at ", min," then!\n")
-        print("What is the end?")
+        print("Great! We will start at", min,"then!\n")
+        print("What is the end number?")
         max = get_int()
-        print("Great! We will end with ", max,", excellent choice!\n")
+        while max <= min:
+            print("That doesn't seem right... the game can't end before it starts!\nMake sure you're writing a number that is larger than", min,"!\n\nLet's try that again! When should the game end? ")
+            max = get_int()
+        print("Great! We will end with", max,", excellent choice!\n")
         os.system("pause")
         break
 
@@ -41,6 +43,7 @@ def get_int():
     while True:
         try:
             num = int(input())
+            print()
             return num
         except ValueError:
             print("\nSorry, I didn't understand!\nYou should reply with an Integer (whole number)!\n")
